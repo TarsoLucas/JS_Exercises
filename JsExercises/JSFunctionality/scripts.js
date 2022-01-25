9664f408a958bee4ab65fa989968312fad16d766
Tarso <tarsolucas@gmail.com>
2022-01-25 18:16:55 -0300

    fixing 'JSFunctionality'

--- JsExercises/JSFunctionality/scripts.js
+++ JsExercises/JSFunctionality/scripts.js
@@ -1,16 +1,21 @@
 const buttonOpenModal = document.getElementById('openModal')
-
-const modalWrapper = document.querySelector('modal-wrapper')
+console.log(buttonOpenModal)
+const modalWrapper = document.querySelector('.modal-wrapper')
+console.log(modalWrapper)
 
 buttonOpenModal.onclick = function() {
-    modalWrapper.classList.remove('invisible')
+    //modalWrapper.classList.remove('invisible')
+    console.log(modalWrapper.classList.contains('invisible'))
+    if(modalWrapper.classList.contains('invisible')){
+        modalWrapper.classList.remove('invisible')
+    }
 }
 
 document.addEventListener('keydown', function(event) {
     const isEscKey = event.key === 'Escape'
     
-    if(modalWrapper.classList('insivible') == false){
-        if(IsEscKey){
+    if(!modalWrapper.classList.contains('invisible')){
+        if(isEscKey){
             modalWrapper.classList.add('invisible')
         }
     }
